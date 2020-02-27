091671b703c47d308cd4096214cf5edcdc224aa0
John Leonardo <johnleonardo@csus.edu>
2020-02-27 15:23:24 -0800

    strip away unnecessary things from boilerplate

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
 import Application from './components/Application';
-import store from './store';
 
 // Create main element
 const mainElement = document.createElement('div');
@@ -13,11 +11,7 @@ document.body.appendChild(mainElement);
 // Render components
 const render = (Component: () => JSX.Element) => {
     ReactDOM.render(
-        <AppContainer>
-            <Provider store={store}>
-                <Component />
-            </Provider>
-        </AppContainer>,
+        <Application/>,
         mainElement
     );
 };
