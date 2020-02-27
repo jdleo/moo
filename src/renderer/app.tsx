b94364dd2813c2abaea2fee2a4a4a1a33543e365
John Leonardo <johnleonardo@csus.edu>
2020-02-27 15:47:50 -0800

    initial styles for app.tsx and Application.tsx

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -5,6 +5,13 @@ import Application from './components/Application';
 
 // Create main element
 const mainElement = document.createElement('div');
+
+// set body styles here
+document.body.style.backgroundColor = "#f0e3ff";
+document.body.style.height = "100%";
+document.body.style.width = "100%";
+
+// add main element to body
 document.body.appendChild(mainElement);
 
 // Render components
--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -2,9 +2,22 @@ import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
 const Application = () => (
-    <div>
-        Hello World!
+    <div style={styles.container}>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,900&display=swap" rel="stylesheet"></link>
+        <h1 style={styles.h1}>Moo(d)</h1>
     </div>
 );
+
+const styles = {
+    container: {
+        padding: "30px",
+        paddingTop: "0px"
+    },
+    h1: {
+        fontFamily: "Nunito",
+        fontWeight: 700,
+        color: "#3e206d"
+    }
+}
 
 export default hot(Application);
