57f28a3d32fadaab2cf8fbc342ff8dfdcdd7698d
John Leonardo <johnleonardo@csus.edu>
2020-02-27 17:30:48 -0800

    style Button.tsx

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -4,25 +4,29 @@ import Button from './Button';
 
 // functional component
 const Application = () => {
-
     // helper method to render date in pretty format
     const renderDate: () => string = () => {
         // get current date
         const date = new Date();
-        
+
         // get date elements needed
-        const day = date.toLocaleString('en-us', {weekday: "long"});
-        const month = date.toLocaleString('en-us', {month: "long"});
+        const day = date.toLocaleString('en-us', { weekday: 'long' });
+        const month = date.toLocaleString('en-us', { month: 'long' });
 
         // build string and return
         return `${day}, ${date.getDate()}. ${month}`;
-    }
+    };
+
+    // helper method to handle refresh click
+    const handleRefresh: () => void = () => {
+        console.log('refresh');
+    };
 
     return (
         <div style={styles.container}>
             <h1 style={styles.h1}>Moo(d)</h1>
             <h3 style={styles.h3}>{renderDate()}</h3>
-            <Button/>
+            <Button title={'REFRESH'} onClick={() => handleRefresh()} />
         </div>
     );
 };
--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -1,16 +1,64 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-// functional component
-const Button = () => {
-    return (
-        <button/>
-    );
-};
+// interface for props for this component
+interface IProps {
+    title: string;
+    onClick: () => void;
+}
 
-// for styling
-const styles = {
-    
+// functional component
+const Button: React.FC<IProps> = ({ title, onClick }) => {
+    // for button background color property
+    const [backgroundColor, setBackgroundColor] = React.useState('#fff');
+
+    // for button color property
+    const [color, setColor] = React.useState('#3e206d');
+
+    // for button border radius property
+    const [borderRadius, setBorderRadius] = React.useState('10px');
+
+    // for handling onHover event
+    const onHover: () => void = () => {
+        // invert on hover
+        setBackgroundColor('#3e206d');
+        setColor('#fff');
+        setBorderRadius('20px');
+    };
+
+    // for handling onBlur event
+    const onBlur: () => void = () => {
+        // set back to normal
+        setBackgroundColor('#fff');
+        setColor('#3e206d');
+        setBorderRadius('10px');
+    };
+
+    // for styling
+    const styles = {
+        button: {
+            fontFamily: 'Nunito',
+            fontWeight: 700,
+            fontSize: '14px',
+            backgroundColor: backgroundColor,
+            color: color,
+            padding: '10px 40px',
+            borderRadius: borderRadius,
+            border: 'none',
+            outline: 'none'
+        }
+    };
+
+    return (
+        <button
+            onMouseEnter={() => onHover()}
+            onMouseLeave={() => onBlur()}
+            style={styles.button}
+            onClick={onClick}
+        >
+            {title}
+        </button>
+    );
 };
 
 export default hot(Button);
