25a5d0f655d1f053aab49a9370502d63a11e7016
John Leonardo <johnleonardo@csus.edu>
2020-02-27 16:07:16 -0800

    some styling and headers

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -11,6 +11,13 @@ document.body.style.backgroundColor = "#f0e3ff";
 document.body.style.height = "100%";
 document.body.style.width = "100%";
 
+// for custom fonts
+const fonts = document.createElement('link');
+fonts.href = "https://fonts.googleapis.com/css?family=Nunito:400,600,700,900&display=swap";
+fonts.rel = "stylesheet";
+document.body.appendChild(fonts);
+
+
 // add main element to body
 document.body.appendChild(mainElement);
 
--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,23 +1,46 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
+import { render } from 'react-dom';
 
-const Application = () => (
-    <div style={styles.container}>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,900&display=swap" rel="stylesheet"></link>
-        <h1 style={styles.h1}>Moo(d)</h1>
-    </div>
-);
+// functional component
+const Application = () => {
 
+    // helper method to render date in pretty format
+    const renderDate: () => string = () => {
+        // get current date
+        const date = new Date();
+        
+        // get date elements needed
+        const day = date.toLocaleString('en-us', {weekday: "long"});
+        const month = date.toLocaleString('en-us', {month: "long"});
+
+        // build string and return
+        return `${day}, ${date.getDate()}. ${month}`;
+    }
+
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.h1}>Moo(d)</h1>
+            <h3 style={styles.h3}>{renderDate()}</h3>
+        </div>
+    );
+};
+
+// for styling
 const styles = {
     container: {
-        padding: "30px",
-        paddingTop: "0px"
+        padding: '30px',
+        paddingTop: '0px'
     },
     h1: {
-        fontFamily: "Nunito",
+        fontFamily: 'Nunito',
         fontWeight: 700,
-        color: "#3e206d"
+        color: '#3e206d'
+    },
+    h3: {
+        fontFamily: 'Nunito',
+        color: '#916dd5'
     }
-}
+};
 
 export default hot(Application);
